79abd545e946f968fb006f56b6ce04c9ea563cf6
Mauricio Belforte <belymau@hotmail.com>
2024-09-24 00:19:50 -0300

    Se agrego el boton modificar, para realizar cambios en los gastos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 
 
 
-
 const arregloGastos = [
 
 
 ]
 
-function clickBoton() {
+
+let indiceEdicion = -1; // Variable global para guardar el índice del gasto a modificar
+
+
+function clickAgregarGasto() {
     const inputNombreGasto = document.getElementById("nombreGasto");
     const inputValorGasto = document.getElementById("valorGasto");
     const inputDescripcionGasto = document.getElementById("descripcionGasto");
 
     let gasto;
 
-    nombreGasto = inputNombreGasto.value;
-    valorGasto = parseFloat(inputValorGasto.value);
-    descripcionGasto = inputDescripcionGasto.value;
+    const nombreGasto = inputNombreGasto.value;
+    const valorGasto = parseFloat(inputValorGasto.value);
+    const descripcionGasto = inputDescripcionGasto.value;
 
     alertaGastoElevado(valorGasto);
     gasto = { nombre: nombreGasto, valor: valorGasto, descripcion: descripcionGasto };
@@ -32,9 +35,30 @@ function clickBoton() {
 
 
     actualizarListaGastos();
-    limpiar();
+    limpiarCampos();
 }
 
+
+function clickGuardarCambiosGasto() {
+
+    if (indiceEdicion >= 0) {
+        let nombre = document.getElementById("nombreGasto").value;
+        let valor = parseFloat(document.getElementById("valorGasto").value);
+        let descripcion = document.getElementById("descripcionGasto").value;
+
+        // Actualizar el gasto existente
+        arregloGastos[indiceEdicion] = { nombre, valor, descripcion };
+        indiceEdicion = -1; // Resetear el índice de edición
+
+        // Mostrar el botón de agregar y ocultar el de actualizar
+        document.getElementById("botonAgregarGasto").style.display = "inline";
+        document.getElementById("botonGuardarCambiosGasto").style.display = "none";
+
+        actualizarListaGastos(); // Actualizar la lista de gastos
+        limpiarCampos(); // Limpiar los campos de entrada
+    }
+
+}
 
 
 function actualizarListaGastos() {
@@ -44,7 +68,7 @@ function actualizarListaGastos() {
     let listaHTML = '';
     let totalGastos = 0;
     arregloGastos.forEach((cadaGasto, indice) => {
-        listaHTML = listaHTML + `<li><p>${cadaGasto.nombre}: $${cadaGasto.valor}</p> <span>Descripcion: ${cadaGasto.descripcion}</span><button onclick="botonEliminarGasto(${indice})">Eliminar </button> </li>`;
+        listaHTML = listaHTML + `<li><p>${cadaGasto.nombre}: $${cadaGasto.valor}</p> <span>Descripcion: ${cadaGasto.descripcion}</span> <button onclick="botonEliminarGasto(${indice})">Eliminar </button> <button onclick="botonModificarGasto(${indice})">Modificar </button> </li>`;
         totalGastos = totalGastos + cadaGasto.valor;
     })
 
@@ -55,7 +79,7 @@ function actualizarListaGastos() {
 }
 
 
-function limpiar() {
+function limpiarCampos() {
 
     document.getElementById("nombreGasto").value = '';
     document.getElementById("valorGasto").value = '';
@@ -71,6 +95,24 @@ function botonEliminarGasto(indice) {
     }
 }
 
+
+function botonModificarGasto(indice) {
+
+    // Obtener el gasto correspondiente al índice
+    let gasto = arregloGastos[indice];
+
+    // Asignar los valores del gasto a los campos de entrada
+    document.getElementById("nombreGasto").value = gasto.nombre;
+    document.getElementById("valorGasto").value = gasto.valor;
+    document.getElementById("descripcionGasto").value = gasto.descripcion;
+
+    indiceEdicion = indice; // Guardar el índice del gasto a modificar
+
+    // Mostrar el botón de actualizar y ocultar el de agregar
+    document.getElementById("botonAgregarGasto").style.display = "none";
+    document.getElementById("botonGuardarCambiosGasto").style.display = "inline";
+
+}
 
 
 function alertaGastoElevado(gasto) {
